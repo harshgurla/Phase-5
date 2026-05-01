97dfd5ac1436d2e3c15b0bef74e2ce0a06a1fc75
harsh kumar gurla <harshkumargurla24@navgurukul.org>
2026-05-01 15:11:21 +0530

    frontend push

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+const questionInput = document.getElementById("question");
+const btn = document.getElementById('btn');
+const answer = document.getElementById('answer');
+
+
+// btn.addEventListener("click", () => {
+//     alert("button clicked");
+// })
+
+btn.addEventListener("click", async () => {
+    const question = questionInput.value;
+    if ( !question) return;
+
+    answer.textContent = "Thinking...";
+
+    try {
+        const response = await fetch("http://localhost:3000/api/ask", {
+            method: "POST",
+            headers: { "Content-type": "application/json"},
+            body: JSON.stringify({ question })
+        });
+
+        const data = await response.json();
+        console.log(data)
+        answer.textContent = data.answer;
+    } catch (err) {
+        answer.textContent = "something went wrong";
+        console.log(err)
+    }
+})
+
